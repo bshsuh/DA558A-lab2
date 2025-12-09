9cdb6a3874925030658a9b3903dc94f0d3de25e8
Bisher Suheil <Suhailbisher@gmail.com>
2025-12-09 17:54:00 +0100

    add: contact form script
    Create a script responsible for contact form elements and get relevant elements

---
+++ contactform.js
@@ -0,0 +1,5 @@
+const nameFields = document.getElementsByClassName("name-field");
+const emailField = document.getElementById("email");
+const phoneField = documnet.getElementById("phone");
+const messageField = document.getElementById("message");
+const submitButton = document.getElementsById("submit");
