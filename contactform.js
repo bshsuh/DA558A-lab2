ad80f04cb3db2de1f7c11cd9d2746c1f9f7170bd
Bisher Suheil <Suhailbisher@gmail.com>
2025-12-13 13:54:37 +0100

    add: inputfields class
    Add a new class to modulate html element handling

--- contactform.js
+++ contactform.js
@@ -1,5 +1,31 @@
-const nameFields = document.getElementsByClassName("name-field");
+class InputField {
+    constructor(id, regexRule, currentValidation = true) 
+    {
+        this.id = id;
+        this.element = document.getElementById(id);
+        this.regexRule = regexRule;
+        this.regexRules = [regexRule];
+        this.currentValidation = currentValidation;
+    }
+
+    getElement() {
+        return this.element;
+    }
+
+    flagValidation() {
+        this.currentValidation = !this.currentValidation;
+    }
+
+    addRegexRule(newRule) {
+        this.regexRules.push(newRule);
+    }
+
+}
+
+// Create references for first name and last name fields
+// Regular expression /^[A-Za-z]*$/ ensures fields may only contain letters 
+const textFields = [new InputField("first-name", /^[A-Za-z]*$/), new InputField("last-name", /^[A-Za-z]*$/)]
 const emailField = document.getElementById("email");
-const phoneField = documnet.getElementById("phone");
+const phoneField = document.getElementById("phone");
 const messageField = document.getElementById("message");
-const submitButton = document.getElementsById("submit");
+const submitButton = document.getElementById("submit");
